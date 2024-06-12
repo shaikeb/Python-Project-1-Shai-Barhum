ef0c1d96c3f1e39429b0d0b3d73ddb58363a75aa
shaikeb <shay650@gmail.com>
2024-06-12 18:08:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,11 +61,13 @@ else:
             city_from_list_striped=line.strip()
             cities.append(city_from_list_striped)
     results_button=[]
+    results_button_another = []
     results_button.append(f'default city: {cities[0]}')
     for i in range (1,int(len(cities))):
         results_button.append(cities[i])
-    results_button.append('Another city')
+    results_button_another.append('Another city')
     choises=st.radio('Choose City', results_button)
+    other_choises = st.radio('My city is not here', results_button_another)
     if choises==f'default city: {cities[0]}':
         new_city_name = cities[0]
         ff = open('defaulttemp.txt', 'r')
@@ -96,7 +98,7 @@ else:
             st.write(f'The temperature at {new_city_name} is', 1.8 * int((results['main']['temp']) - 273) + 32,
                      "Fahrenheit Degrees")
         st.write(f'The humidity percent at {new_city_name} is', results['main']['humidity'], "%")
-    elif choises=='Another City':
+    if other_choises:
         ncn=st.text_input('Please type the city name:')
         url3 = f"https://api.openweathermap.org/data/2.5/weather?q={ncn}&appid=80dfc5415edfd995583e08d0977bf427"
         r = rq.get(url3)
