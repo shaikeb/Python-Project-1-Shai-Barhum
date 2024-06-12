a2652059d98b6edbfe9cb65e0e02d81159617f7b
shaikeb <shay650@gmail.com>
2024-06-12 20:39:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,12 +48,14 @@ if (city=='Default'):
         else:
             st.write(f'The temperature at {city_name} is', 1.8 * int((results['main']['temp']) - 273) + 32, "Fahrenheit Degrees")
         st.write(f'The humidity percent at {city_name} is', results['main']['humidity'], "%")
-        with open('default.txt', 'w') as f:
-            print(city_name)
-            print(city_name_1)
-            print(city_name_2)
-        with open('defaulttemp.txt', 'w') as ff:
-            print(type)
+        f = open('default.txt', 'w'):
+        print(city_name, file=f)
+        print(city_name_1, file=f)
+        print(city_name_2, file=f)
+        f.close()
+        ff = open('defaulttemp.txt', 'w')
+        print(type, file=ff)
+        ff.close()
 else:
     cities=[]
     with open('default.txt', 'r') as f:
